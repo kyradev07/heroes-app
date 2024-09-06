53d76929ee014748a2f7cca2665d59969cac5d3e
Kyra <kyra.dev.07@gmail.com>
2024-09-05 22:39:20 -0500

    Se cambia definición de observable por signal en la ruta del servicio

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { HeroesService } from "../../services/heroes.service";
 import { ActivatedRoute, Router } from "@angular/router";
-import { catchError, Observable, of, switchMap } from "rxjs";
+import { catchError, of, switchMap } from "rxjs";
 import { Hero } from "../../interfaces/hero.interface";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-hero-page',
@@ -15,17 +16,16 @@ export class HeroPageComponent {
   private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private router: Router = inject(Router);
 
-  hero$: Observable<Hero | null>;
-
+  heroSignal: Signal<Hero | undefined>;
 
   constructor() {
-    this.hero$ = this.activatedRoute.params.pipe(
+    this.heroSignal = toSignal(this.activatedRoute.params.pipe(
       switchMap((params) => this.heroService.getHeroById(params['id'])),
       catchError(() => {
         this.router.navigate(['/heroes/list']).then();
-        return of(null);
+        return of(undefined);
       })
-    );
+    ));
   }
 
   goBack(): void {
