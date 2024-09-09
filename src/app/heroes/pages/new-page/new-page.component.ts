d5c12630e0dc0d5d6189a6d8f3199858798a38a4
Kyra <kyra.dev.07@gmail.com>
2024-09-08 21:10:32 -0500

    Se agrega consulta de héroe para edición, se ajustan mensajes en el formulario para mostrar si está creando o editando, se agrega snackbar de confirmación, se agrega redireccionamiento de rutas

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,16 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { Hero, Publisher, PublisherOptions } from "../../interfaces/hero.interface";
 import { FormControl, FormGroup } from "@angular/forms";
 import { HeroesService } from "../../services/heroes.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { switchMap } from "rxjs";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   private heroService: HeroesService = inject(HeroesService);
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private router: Router = inject(Router);
+  private snackBar: MatSnackBar = inject(MatSnackBar);
+
+  isEditing: boolean;
+  heroForm: FormGroup;
+  currentHero: WritableSignal<Hero>;
 
   publisherOptions: PublisherOptions[] = [
     {
@@ -23,25 +33,53 @@ export class NewPageComponent {
     }
   ]
 
-  heroForm: FormGroup = new FormGroup({
-    id: new FormControl(''),
-    superhero: new FormControl<string>('', { nonNullable: true }),
-    alter_ego: new FormControl<string>(''),
-    publisher: new FormControl<Publisher>(Publisher.DCComics),
-    first_appearance: new FormControl<string>(''),
-    characters: new FormControl<string>(''),
-    alt_img: new FormControl<string>(''),
-  });
+  constructor() {
+    this.isEditing = this.router.url.split('/')[2] === 'edit';
 
-  onSubmit(): void {
+    this.heroForm = new FormGroup({
+      id: new FormControl(''),
+      superhero: new FormControl<string>('', { nonNullable: true }),
+      alter_ego: new FormControl<string>(''),
+      publisher: new FormControl<Publisher>(Publisher.DCComics),
+      first_appearance: new FormControl<string>(''),
+      characters: new FormControl<string>(''),
+      alt_img: new FormControl<string>(''),
+    });
 
-    if (this.heroForm.valid) {
-      this.heroService.updateHero(this.currentHero).subscribe(heroR => alert(`Hero ${heroR.id} saved`));
+    this.currentHero = signal<Hero>(this.heroForm.value);
+
+    this.heroForm.valueChanges.subscribe(value => {
+      this.currentHero.set(value);
+    })
+  }
+
+  ngOnInit(): void {
+    if (this.isEditing) {
+      this.activatedRoute.params.pipe(
+        switchMap((params) => this.heroService.getHeroById(params['id'])),
+      ).subscribe({
+        next: (value: Hero) => {
+          this.heroForm.reset(value);
+        },
+        error: () => this.router.navigate(['/heroes/list']).then(),
+      });
     }
   }
 
-  get currentHero(): Hero {
-    return this.heroForm.value as Hero;
+  onSubmit(): void {
+
+    if (this.currentHero().id) {
+      this.heroService.updateHero(this.currentHero()).subscribe(heroR => this.showSnackBar(`Hero ${heroR.superhero} updated!`));
+    } else {
+      this.heroService.addHero(this.currentHero()).subscribe(heroR => {
+        this.showSnackBar(`Hero ${heroR.superhero} added!`);
+        this.router.navigate(['/heroes/list']).then();
+      });
+    }
+  }
+
+  private showSnackBar(message: string): void {
+    this.snackBar.open(message, 'Done', { duration: 3000 });
   }
 
 }
