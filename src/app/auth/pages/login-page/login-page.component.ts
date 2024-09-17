9b6e8bf26540e62bc590b2f09490eb270a5d3513
Kyra <kyra.dev.07@gmail.com>
2024-09-17 01:14:15 -0500

    Se crea formulario para obtener los datos de ingreso y se asocia a la vista html, se agrega snackbar para mostrar mensajes

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LoginPageComponent {
+  private authService: AuthService = inject(AuthService);
+  private snackBar: MatSnackBar = inject(MatSnackBar);
+  private formBuilder: FormBuilder = inject(FormBuilder);
+  private router: Router = inject(Router);
 
+  loginForm;
+
+  constructor() {
+    this.loginForm = this.formBuilder.nonNullable.group({
+      userId: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  onLogin(): void {
+    if (this.loginForm.invalid) {
+      this.snackBar.open("Forms fields are required", "OK", { duration: 3000 });
+      return;
+    }
+
+    const password = btoa(this.loginForm.controls.password.value);
+    this.authService.login(this.loginForm.controls.userId.value, password).subscribe({
+      next: user => this.router.navigateByUrl(`/`),
+      error: () => this.snackBar.open('User Id not found', 'Done', { duration: 3000 })
+    });
+  }
 }
+
