7f32276b8341d596a4a692a9ac7ad8f63dc6dc60
Kyra <kyra.dev.07@gmail.com>
2024-09-22 14:50:27 -0500

    Se agrega crea servicio para validar si el usuario está o no autenticado

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, signal, WritableSignal } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
 import { User } from "../interfaces/user.interface";
-import { Observable, tap } from "rxjs";
+import { catchError, map, Observable, of, tap } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,7 @@ export class AuthService {
   }
 
   getCurrentUser(): User | null {
+    console.log(this.user())
     return this.user() === null
       ? null
       : structuredClone(this.user());
@@ -26,6 +27,20 @@ export class AuthService {
     return this.http.get<User>(`${this.url}/users/${userId}`).pipe(
       tap((user: User) => this.user.set(user)),
       tap((user: User) => localStorage.setItem('token', user.id.toString()))
+    );
+  }
+
+  checkAuth(): Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.url}/users/${token}`).pipe(
+      tap((user: User) => this.user.set(user)),
+      map(() => true),
+      catchError(() => of(false))
     );
   }
 
